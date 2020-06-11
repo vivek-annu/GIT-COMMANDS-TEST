0bb78da6a8ce949684513eb26683f3ec1e7db0b2
VIVEK KUMAR PATEL <ai.springboot@gmail.com>
2020-06-11 08:30:12 +0530

    changes

--- techmahendra/src/main/java/com/techmahendra/service/Vivek.java
+++ techmahendra/src/main/java/com/techmahendra/service/Vivek.java
@@ -1,7 +1,13 @@
 package com.techmahendra.service;
 
 public class Vivek {
-    public static void main(String[] args) {
-        System.out.println("vivek");
+
+
+  public static void main(String[] args) {
+
+
+        System.out.println("ramesh");
+ System.out.println("ramesh");
     }
+    
 }
